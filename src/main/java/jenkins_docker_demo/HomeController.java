476559c2eb98f61bebc639e6c6e9687b2d2a33a0
Fanantenana-ds <fanantenana7170@gmail.com>
2026-07-29 00:27:28 +0300

    Ajout Dockerfile et API Hello Jenkins Docker

---
+++ src/main/java/jenkins_docker_demo/HomeController.java
@@ -0,0 +1,13 @@
+package jenkins_docker_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Jenkins Docker!";
+    }
+}
